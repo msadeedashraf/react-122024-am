8fdfeb198c00456b95c7b7c9f65bd9a9199e5101
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-12-16 17:32:21 +0000

    .

--- day11/jobzila-v1/src/components/Navbar.jsx
+++ day11/jobzila-v1/src/components/Navbar.jsx
@@ -5,15 +5,27 @@ const Navbar = () => {
     <header>
     <h1>JobZila</h1>
     <nav>
-      <ul>
-        <li><a href="index.html">Home</a></li>
-        <li><a href="job_search.html">Job Search</a></li>
-        <li><a href="job_listing.html">Job Listing</a></li>
-        <li><a href="terms.html">Terms of Service</a></li>
-        <li><a href="privacy.html">Privacy Policy</a></li>
-        <li><a href="contact.html">Contact Us</a></li>
-      </ul>
-    </nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/job_search">Job Search</a>
+            </li>
+            <li>
+              <a href="/job_listing">Job Listing</a>
+            </li>
+            <li>
+              <a href="/term_service">Terms of service</a>
+            </li>
+            <li>
+              <a href="/privacy">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/contact_us">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
   </header>
   )
 }
